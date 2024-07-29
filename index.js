848d37187f2fb0a2bab7708fd3612c0cce4537b8
cristianalma <cristianeltem315@gmail.com>
2024-07-29 10:11:40 -0900

    Subida de templates

--- index.js
+++ index.js
@@ -6,12 +6,33 @@ const routes = require('./routes');//llamado rutas
 
 const app = express();// cremaos una nueva instancia
 const port = 3000;// puerto de salida 
-app.use(express.static('public'));
-app.use(express.json()) //configurar tipo de dato js
 
+
+app.use(express.json()) //configurar tipo de dato js
 app.use('/api', routes);//configura la url base y tutas
 
-app.listen(port, ()=>{//ejecuta la api po el puerto 3000
-    //mensaje 
+app.use(express.static('public'));
+
+//URL base y rutas 
+app.use('/api', routes);
+
+//404
+app.use((req, res, next) => {
+    res.status(404).send('page not found');
+});
+
+//errores
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Algo salió mal!');
+});
+
+app.listen(port, () => { //API por el puerto 3001
     console.log(`Servidor corriendo en el puerto ${port}`);
-})
+});
+
+
+
+
+
+
